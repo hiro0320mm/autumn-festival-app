ed52b44cec34a08ac84ada46a0ba53b0f79b33a5
hiro0320mm <h.mikawa@gmail.com>
2026-09-14 09:24:06 +0900

    ADD ViteConfigにpreview追加

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
-      }
-    }
-  }
-})
+      "/api": "http://localhost:8080",
+    },
+  },
+  preview: {
+    allowedHosts: ["autumn-festival-app.onrender.com"],
+  },
+});
